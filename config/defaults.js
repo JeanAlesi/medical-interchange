14aa7c1d3a9b34462832ae28e5180df17d990652
ggerrein <ggerrein@gmail.com>
2015-11-03 12:39:11 -0200

    added remote db url

--- config/defaults.js
+++ config/defaults.js
@@ -2,6 +2,7 @@ module.exports = {
     address : '127.0.0.1',
 
     db : {
-        url : 'mongodb://localhost/database'
+        // url : 'mongodb://localhost/database'
+        url : 'mongodb://heroku_dv90v0mt:u9lcb9t53c7srfghd8ja0q8tid@ds049744.mongolab.com:49744/heroku_dv90v0mt'
     }
 };
