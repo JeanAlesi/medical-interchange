57e2a1c3fc60223f6c320b19ea4881a96fc52e47
ggerrein <ggerrein@gmail.com>
2015-11-03 13:40:18 -0200

    back to localhost for db connection

--- config/defaults.js
+++ config/defaults.js
@@ -1,5 +1,6 @@
 module.exports = {
+    address : '127.0.0.1',
     db : {
-        url : 'mongodb://heroku_dv90v0mt:u9lcb9t53c7srfghd8ja0q8tid@ds049744.mongolab.com:49744/heroku_dv90v0mt'
+        url : 'mongodb://localhost/database'
     }
 };
