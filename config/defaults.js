bb3b5672ef81afab2b4b9278b2c272c2aefed7c9
ggerrein <ggerrein@gmail.com>
2015-11-03 13:17:23 -0200

    removed localhost

--- config/defaults.js
+++ config/defaults.js
@@ -1,7 +1,5 @@
 module.exports = {
-    address : '127.0.0.1',
-
     db : {
         url : 'mongodb://heroku_dv90v0mt:u9lcb9t53c7srfghd8ja0q8tid@ds049744.mongolab.com:49744/heroku_dv90v0mt'
-    } 
+    }
 };
