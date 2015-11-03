c4432d9724047b573346ee5d010b6871b310c515
ggerrein <ggerrein@gmail.com>
2015-11-03 12:44:38 -0200

    removed comment

--- config/defaults.js
+++ config/defaults.js
@@ -2,7 +2,6 @@ module.exports = {
     address : '127.0.0.1',
 
     db : {
-        // url : 'mongodb://localhost/database'
         url : 'mongodb://heroku_dv90v0mt:u9lcb9t53c7srfghd8ja0q8tid@ds049744.mongolab.com:49744/heroku_dv90v0mt'
     }
 };
