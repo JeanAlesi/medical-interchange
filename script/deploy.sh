6a3a496015f76589533f62398e94c327f66eb212
Tony Allen <tallen@nutanix.com>
2015-11-10 03:32:12 -0500

    Disable host key checking in deploy script.

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,7 @@
 # This script deploys the medical interchange site.
 
 AWS_KEY='/etc/starfleet/starfleet.pem'
-ssh -i $AWS_KEY ubuntu@starfleet.stormbeard.net "
+ssh -o StrictHostKeyChecking=no -i $AWS_KEY ubuntu@starfleet.stormbeard.net "
   cd /home/ubuntu/medical-interchange;
   git pull;
   npm install --production;
