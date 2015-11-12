26d660bdb090897874867b207d9df6961285c3a4
wewilli1 <wewilliamson@windstream.net>
2015-11-12 18:59:42 -0500

    Initial commit of quality grading branch.

--- models/item.js
+++ models/item.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Qualities = ['new', 'new expired', 'used like new', 'used great', 'used good', 'used fair', 'used poor'];
 
 var Item = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     category: { type: String, required: true },
-    condition: { type: String, required: true }
+    condition: { type: String , enum: Qualities, required: true }
 });
 
 module.exports = mongoose.model('Item', Item);
