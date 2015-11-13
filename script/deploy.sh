070260ba1636c53dda7f679cdadb089afdfd9097
Tony Allen <tallen@nutanix.com>
2015-11-13 00:04:31 -0500

    Adding git fetch to deploy script prior to forced sync.

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,7 +5,8 @@
 AWS_KEY='/etc/starfleet/starfleet.pem'
 ssh -o StrictHostKeyChecking=no -i $AWS_KEY ubuntu@starfleet.stormbeard.net "
   cd /home/ubuntu/medical-interchange;
-  git reset --hard origin/master
+  git fetch;
+  git reset --hard origin/master;
   npm install --production;
   sudo forever restartall;
 "
