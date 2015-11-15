d84a48b483f1128ac3a336b02090fddfef4e71a4
Tony Allen <tallen@nutanix.com>
2015-11-15 10:06:11 -0800

    TEST: /items/create POST Missing Fields

--- test/test_item_routes.js
+++ test/test_item_routes.js
@@ -33,7 +33,6 @@ describe('Routes', function() {
             });
     });
 
-    // wew to do: Check with Tony why this test fails when using done().
     // /items/create PUT No Errors
     it('/items/create POST No Errors', function(done){
         var unique_name = 'TESTING_PUT_NO_ERRORS';
@@ -43,7 +42,6 @@ describe('Routes', function() {
             .field('description','2005 Model Year')
             .field('category','Hospital Equipment')
             .field('condition','Used')
-            //.send('submit', 'Create')
             .end(function(err, res){
             });
         chai.request(server)
@@ -56,27 +54,27 @@ describe('Routes', function() {
           });
     });
 
-    // /items/create PUT With Errors
-    it.skip('/items/create POST With Errors', function(done){
+    // /items/create PUT Missing Fields
+    it('/items/create POST Missing Fields', function(done){
+        var unique_name = 'TESTING_PUT_MISSING_FIELD';
+        var missing_title_err = "Title is required"
+        var missing_description_err = "Description is required";
+        var missing_category_err = "Category is required";
         chai.request(server)
-        // post with missing condition fields
-            .post('/items/create/')
-            .field('title','XRay Machine')
-            .field('description','2005 Model Year')
-            .field('category','Hospital Equipment')
-            .end(function(err, res) {
-                //console.log("Processing response in /items/create PUT With Errors");
-                expect(err).to.be.null;
-                expect(res).to.have.status(404);
-                //var error_index = res.text.indexOf("Condition is required");
-                //var success = false;
-                //if (error_index > -1)
-                //{
-                //    success = true;
-                //}
-                //expect(success).to.equal(true);
-                //expect(res.text).to.contain('Condition is required');
-                done();
+            .post('/items/create')
+            // Missing title.
+            // Missing description.
+            // Missing category.
+            .end(function(err, res){
+            expect(err).to.be.null;
+            expect(res).to.have.status(200);
+            var title_index = res.text.indexOf(missing_title_err);
+            expect(title_index).to.not.equal(-1);
+            var description_index = res.text.indexOf(missing_description_err);
+            expect(description_index).to.not.equal(-1);
+            var category_index = res.text.indexOf(missing_category_err);
+            expect(category_index).to.not.equal(-1);
+            done();
             });
     });
 
