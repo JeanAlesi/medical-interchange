eae91d860b5e06ea4e11d2113977d1294135c627
ggerrein <ggerrein@gmail.com>
2015-12-05 17:25:14 -0800

    associate user to item creation

--- models/item.js
+++ models/item.js
@@ -6,7 +6,8 @@ var Item = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     category: { type: String, required: true },
-    condition: { type: String, required: true }
+    condition: { type: String, required: true },
+    user: { type: String, required: true }
 });
 
 var ItemModel = mongoose.model('Item', Item);
