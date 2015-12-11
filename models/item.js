8011ff5cf1ab9fc97d5facf74c041bee6cccebe8
Antonio Recalde Russo <recalderusso@g.harvard.edu>
2015-12-10 20:21:25 -0800

    added map

--- models/item.js
+++ models/item.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ItemConditions = ["New", "New - Expired", "Used - Like new", "Used - Great", "Used - Good", "Used - Fair", "Used - Poor" ];
 
+
 var Item = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     category: { type: String, required: true },
     condition: { type: String, required: true },
-    user: { type: String, required: true }
+    user: { type: String, required: true },
+    location: { type: String, required: true }
 });
 
 var ItemModel = mongoose.model('Item', Item);
